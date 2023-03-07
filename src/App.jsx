54249af4af18efb4aec98bf16a5478b893000320
augusto pezzutti <pezzupro@gmail.com>
2023-03-06 23:19:39 -0300

    primer push

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Button from './components/Button'
+
+function App() {
+  return (
+   <div className='Container'>
+    <div className='Container-1'>
+      <h1>Hola mundo</h1>
+    </div>
+    <Button />
+
+
+   </div>
+  )
+}
+
+export default App
